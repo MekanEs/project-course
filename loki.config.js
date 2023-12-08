815e3e64149258af9cab6d7e42cf1953923117b0
MekanEs <Mekan.es1997@gmail.com>
2023-12-08 18:59:31 +0300

    feat: crerate loki.config.js file  change loki config

---
+++ loki.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    diffingEngine: 'pixelmatch',
+    chromeEnableAnimations: false,
+    verboseRenderer: true,
+    chromeFlags:
+        '--headless --disable-gpu --hide-scrollbars --wm-window-animations-disabled',
+    configurations: {
+        'chrome.laptop': {
+            target: 'chrome.app',
+            width: 1366,
+            height: 768,
+            deviceScaleFactor: 1,
+            mobile: false,
+        },
+        'chrome.iphone7': {
+            target: 'chrome.app',
+            preset: 'iPhone 7',
+        },
+    },
+};
