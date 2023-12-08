0fbf2cc286231bbe38c8c617d03faa3fa0e0eed5
MekanEs <Mekan.es1997@gmail.com>
2023-12-08 22:35:47 +0300

    fix: try to fix loki tests

--- loki.config.js
+++ loki.config.js
@@ -1,13 +1,18 @@
 module.exports = {
+    diffingEngine: 'pixelmatch',
     verboseRenderer: true,
     configurations: {
         'chrome.laptop': {
             target: 'chrome.app',
             width: 1366,
             height: 768,
-            deviceScaleFactor: 1,
+            deviceScaleFactor: 2,
             mobile: false,
             preset: 'Retina Macbook Pro 15',
+        },
+        'chrome.iphone7': {
+            target: 'chrome.app',
+            preset: 'iPhone 7',
         },
     },
 };
