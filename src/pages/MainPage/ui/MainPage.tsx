91a8a4ea36499231ceeb0adb61ac788e926ad3f2
MekanEs <Mekan.es1997@gmail.com>
2023-12-27 19:20:29 +0300

    chore: remove counter

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,9 @@
-import { Counter } from 'entities/Counter';
 import { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Main: FC = () => {
     const { t } = useTranslation();
-    return (
-        <div>
-            <Counter />
-            {t('Main')}
-        </div>
-    );
+    return <div>{t('Main')}</div>;
 };
 
 export default Main;
